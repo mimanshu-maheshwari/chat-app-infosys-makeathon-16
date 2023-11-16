18bdd087d92e58417a118c448c2812f39f94db70
MIMANSHU MAHESHWARI <maheshwarimimanshu@gmail.com>
2023-11-16 22:29:38 +0530

    add after view init to controls

--- client/src/app/components/player/controls/controls.component.ts
+++ client/src/app/components/player/controls/controls.component.ts
@@ -1,4 +1,4 @@
-import { Output, Component, OnInit, EventEmitter } from '@angular/core';
+import { Output, Component, OnInit, EventEmitter, AfterViewInit } from '@angular/core';
 import { faMicrophone, faMicrophoneSlash, faVideoSlash, faVideo } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
@@ -6,7 +6,7 @@ import { faMicrophone, faMicrophoneSlash, faVideoSlash, faVideo } from '@fortawe
 	templateUrl: './controls.component.html',
 	styleUrls: ['./controls.component.scss']
 })
-export class ControlsComponent implements OnInit {
+export class ControlsComponent implements OnInit, AfterViewInit {
 	@Output('mic-toggle-emmiter') micToggleEmitter: EventEmitter<boolean> = new EventEmitter<boolean>();
 	@Output('video-toggle-emmiter') videoToggleEmitter: EventEmitter<boolean> = new EventEmitter<boolean>();
 
@@ -15,14 +15,22 @@ export class ControlsComponent implements OnInit {
 
 	videoIcon = this.videoState ? faVideo : faVideoSlash;
 	micIcon = this.micState ? faMicrophone : faMicrophoneSlash;
+
 	constructor() {}
 
 	ngOnInit(): void {}
+
+	ngAfterViewInit(): void {
+		this.micToggleEmitter.emit(this.micState);
+		this.videoToggleEmitter.emit(this.videoState);
+	}
+
 	toggleMic() {
 		this.micState = !this.micState;
 		this.micToggleEmitter.emit(this.micState);
 		this.micIcon = this.micState ? faMicrophone : faMicrophoneSlash;
 	}
+
 	toggleVideo() {
 		this.videoState = !this.videoState;
 		this.videoToggleEmitter.emit(this.videoState);
